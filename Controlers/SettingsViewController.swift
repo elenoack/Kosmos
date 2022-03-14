8d9aafc9ffae8517d3239ba013d54d028d277eb9
elenoack <elenanoack@mail.ru>
2022-03-14 11:59:30 +0300

    Create SettingsViewController.swift

---
+++ Controlers/SettingsViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SettingsViewController.swift
+//  Kosmos
+//
+//  Created by mac on 20.11.21.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    // MARK: - Properties
+    @IBOutlet weak var namePlayerTextField: UITextField!
+    @IBOutlet weak var levelSegmentedControl: UISegmentedControl!
+    private let defaults = UserDefaultsStorage()
+    
+    // MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        levelSegmentedControl.selectedSegmentIndex = defaults.level
+        namePlayerTextField.text = defaults.namePlayer
+    }
+    
+    @IBAction func openLeadersList(_ sender: UIButton) {
+        let nextViewController = storyboard!.instantiateViewController(withIdentifier: "LeadersStoryboard")
+        present(nextViewController, animated: false, completion: nil)
+    }
+    
+    @IBAction func save(_ sender: Any) {
+        defaults.level = levelSegmentedControl.selectedSegmentIndex
+        defaults.namePlayer = namePlayerTextField.text 
+        dismiss(animated: false, completion: nil)
+    }    
+}
