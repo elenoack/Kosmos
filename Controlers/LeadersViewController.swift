feec5e3d9c95d91f4f2fbf49eadb8133ed24a6de
elenoack <elenanoack@mail.ru>
2022-03-14 11:59:21 +0300

    Create LeadersViewController.swift

---
+++ Controlers/LeadersViewController.swift
@@ -0,0 +1,54 @@
+//
+//  LeadersViewController.swift
+//  Kosmos
+//
+//  Created by mac on 14.12.21.
+//
+
+import UIKit
+
+class LeadersViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    // MARK: - Properties
+    @IBOutlet weak var tableView: UITableView!
+    private let defaults = UserDefaultsStorage()
+    
+    // MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.backgroundColor = .clear
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "MyCell")
+        tableView.reloadData()
+    }
+    
+    @IBAction func openMenu(_ sender: UIButton) {
+        let nextViewController = storyboard!.instantiateViewController(withIdentifier: "HomeStoryboard")
+        present(nextViewController, animated: false, completion: nil)
+    }
+}
+
+// MARK: - UITableViewDataSource
+extension LeadersViewController {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        defaults.list.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
+        var configuration = cell.defaultContentConfiguration()
+        configuration.text = defaults.list[indexPath.row]
+        cell.textLabel?.text = defaults.list[indexPath.row]
+        cell.backgroundColor = .lightGray
+        return cell
+    }
+}
+
+
+   
+
+   
+
+
