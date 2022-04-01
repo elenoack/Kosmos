c9299207df2d3c21649d73806f175152b54f86c1
elenoack <elenanoack@mail.ru>
2022-04-01 14:07:48 +0300

    Update SettingsViewController.swift

--- Controlers/SettingsViewController.swift
+++ Controlers/SettingsViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class SettingsViewController: UIViewController {
     // MARK: - Properties
+    let blurView: UIVisualEffectView = {
+        let view = UIVisualEffectView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
     @IBOutlet weak var namePlayerTextField: UITextField!
     @IBOutlet weak var levelSegmentedControl: UISegmentedControl!
     private let defaults = UserDefaultsStorage()
@@ -16,10 +21,11 @@ class SettingsViewController: UIViewController {
     // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupBlurView()
         levelSegmentedControl.selectedSegmentIndex = defaults.level
         namePlayerTextField.text = defaults.namePlayer
     }
-    
+        
     @IBAction func openLeadersList(_ sender: UIButton) {
         let nextViewController = storyboard!.instantiateViewController(withIdentifier: "LeadersStoryboard")
         present(nextViewController, animated: false, completion: nil)
@@ -30,4 +36,20 @@ class SettingsViewController: UIViewController {
         defaults.namePlayer = namePlayerTextField.text 
         dismiss(animated: false, completion: nil)
     }    
+}
+
+// MARK: - private
+private extension SettingsViewController {
+    
+    func setupBlurView() {
+        view.addSubview(blurView)
+        blurView.effect = UIBlurEffect(style: .extraLight)
+        blurView.alpha = 0.25
+        NSLayoutConstraint.activate([
+            blurView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            blurView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            blurView.topAnchor.constraint(equalTo: view.topAnchor),
+            blurView.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor)
+        ])
+    }
 }
